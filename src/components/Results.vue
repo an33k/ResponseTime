53298f7bdea58ffc1b3301c1737060109aca79b8
aneek <aneek.khan@outlook.com>
2021-04-25 13:40:12 +0600

    Vue project two

---
+++ src/components/Results.vue
@@ -0,0 +1,38 @@
+<template>
+<p>Reaction time : {{score}}</p>
+<p class="rank">{{rank}}</p>
+  
+</template>
+
+<script>
+export default {
+    props:[
+        'score'
+    ],
+    data(){
+        return{
+            rank:null
+        }
+    },
+    mounted(){
+        if(this.score<250){
+            this.rank='ninja finger'
+        }else if(this.score<400){
+            this.rank='rapid reflexs'
+        }else{
+            this.rank='slow'
+        }
+    }
+
+
+}
+</script>
+
+<style>
+.rank{
+    font-size: 1.4em;
+    color: #0faf87;
+    font-weight: bold;
+}
+
+</style>
